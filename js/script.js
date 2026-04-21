a41ddea3be51376474ddd1d9e7430029e8a17e68
Alan <alanhansenaxel@hotmail.com>
2026-04-21 20:31:34 -0300

    fiter-by-role.js terminado y añadido en script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 import { filterByInput } from "./filter-by-input.js";
 import { toggleLightMode } from "./toggle-light-mode.js";
+import { filterByCategory } from "./filter-by-role.js";
+
 
 filterByInput();
 toggleLightMode();
+filterByCategory();
