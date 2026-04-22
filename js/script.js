c45d36b14328ac3a39693bf82174da035e3c4b51
ValentinDePascale <valentindp9@gmail.com>
2026-04-22 02:43:14 -0300

    feat: add card button functionality and css animations

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import { filterByInput } from "./filter-by-input.js";
 import { toggleLightMode } from "./toggle-light-mode.js";
+import { expandCards } from "./expand-cards.js";
 
 filterByInput();
 toggleLightMode();
+expandCards();
