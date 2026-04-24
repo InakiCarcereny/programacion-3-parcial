5c27242636325b23f0d9fdf70dd5716a0f933671
JoaquinMarcilese <joaquimarci@gmail.com>
2026-04-24 22:12:23 +0000

    Header

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import { filterByInput } from "./filter-by-input.js";
 import { filterByCategory } from "./filter-by-role.js";
 import { renderCards } from "./render-cards.js";
 import { toggleLightMode } from "./toggle-light-mode.js";
+import { floatingHeader } from "./floating-header.js";
 
 renderCards();
 filterByInput();
 toggleLightMode();
 filterByCategory();
 expandCards();
+floatingHeader();
